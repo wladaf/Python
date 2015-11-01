b3c6d75da177332cdd78f6a670ae8fdc63563cf5
wladaf <wladaf@mail.ru>
2015-11-01 19:59:26 +0300

    issue fix

--- randomline.py
+++ randomline.py
@@ -1,16 +1,21 @@
 import random
 
-depth = 4
-n=26
+
+depth = 8
+n=6
+N=26
+done = 0
+
 mass =[[0] for i in range(depth)]
 key = [random.randint(0, n-1) for x in range(depth)]
 result=[]
+message = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 def CreateTable(key = [random.randint(0, n-1) for x in range(depth)]):
 	table = [[0]*n for i in range(depth)]
 	for i in range(depth):
 		for j in range(n):
-			table[i][j]=(j+key[i])%n
+			table[i][j]=(j+key[i])%N
 	print("Key = " + str(key))
 	return table
 
@@ -21,30 +26,32 @@ def Circle(depth):
 			Circle(depth-1)
 		else:
 			result.append([mass[x] for x in range(len(mass))])
+			print(mass)
+		if len(result) > len(message):
+			return 0
 
 def CreateLine():
-	for i in range(10):
+	for i in range(len(message)):
 		print(result[i])
 
 
 def Crypt(message):
+	print(message)
 	message = message.lower()
 	m = list(message)
 	ab = "abcdefghijklmnopqrstuvwxyz"
 	for i in range(len(m)):
 		if m[i] != ' ':
-			print(m[i])
 			ind = ab.find(m[i])
-			print(ind)
 			for j in range(depth):
-				ind = (ind + result[i][j])%n
+				ind = (ind + result[i][j])%N
 			m[i] = ab[ind]
 	print("".join(m))
 
-table = CreateTable([0,0,0,0])
+table = CreateTable([0,0,0,0,0,0,0,1])
 Circle(depth)
-CreateLine()
-Crypt("Hi Vlad")
+#CreateLine()
+Crypt(message)
 
 
 	
